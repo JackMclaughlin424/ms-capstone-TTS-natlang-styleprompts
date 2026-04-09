68ac1af0ebb3eb4bdb209442d06ec4647e499026
sirpighead <jack.mclaughlin24@gmail.com>
2026-04-09 10:55:59 -0400

    sbatch script no key

---
+++ src/style-prompt-generator/short_single_sweep.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+# Declaring Slurm Configuration Options
+
+#SBATCH --job-name=short_single_sweep
+#SBATCH --account=hic-tts
+#SBATCH --partition=tier3
+#SBATCH --time=1-06:00:00
+
+#SBATCH --open-mode=append
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+
+#SBATCH --mail-user=slack:@jdm8943
+#SBATCH --mail-type=ALL
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-task=a100:1
+
+#SBATCH --mem=20g
+
+
+# Loading Software/Libraries
+
+source ~/.conda/etc/profile.d/conda.sh
+conda activate capstone-model
+
+# Env vars
+
+export WANDB_API_KEY=
+export HF_TOKEN=
+
+# launch sweep agent
+
+SHARED_ARGS="--config style-prompt-generator/short_sweep_config.json \
+             --sweep_values style-prompt-generator/short_sweep_values.json \
+             --n_folds 5 \
+             --count 5"
+
+# agent 1 creates the sweep and writes the real ID to a file
+CUDA_VISIBLE_DEVICES=0 python3 style-prompt-generator/sweep.py $SHARED_ARGS 
+
